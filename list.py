40f1346cce56abf46efb04f79b0454e7aca3adcd
Ekawaka <wanyachaeve@gmail.com>
2024-02-21 00:15:16 +0300

    week1 commit

---
+++ list.py
@@ -0,0 +1,15 @@
+numbers = [1,"two",3,4,5, "six",7,8,9, True]
+
+
+fruits = ["apple", "avocado", "mango", "orange"]
+
+cars = []
+
+cars.append ("Toyota")
+cars.append ("Toyota")
+cars.append ("Toyota")
+
+print(fruits)
+print(len(fruits))
+
+size_of_numbers = len(numbers)
---
+++ loops.py
@@ -0,0 +1,30 @@
+for number in range(0, 20):
+    print(number)
+    if (number % 3 == 0):
+        print(f"{number}is multiple of 3!")
+
+# using a for loop, write a python program that prints numbers mutiples of 3
+    
+mixed = [
+
+
+
+        
+numbers = [1,"two",3,4,5, "six",7,8,9, True]   
+
+for number in numbers:
+    if(number == "six"):
+        print("You got the number!")
+
+students = [("Ken", 23), ("Jen", 2)]
+
+def marks():
+    for student in students:
+        for item in student:
+            print(item)
+  
+
+
+
+
+
