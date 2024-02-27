2c7789d423995bb8ae099e8fea7164da2513b4fd
Ekawaka <wanyachaeve@gmail.com>
2024-02-27 22:27:40 +0300

    cli

---
+++ profit.py
@@ -0,0 +1,7 @@
+#Write a python program that calculates the Sales Profit for a business
+
+bp = float(input("Enter buying price"))
+sp = float(input("Enter selling price"))
+
+profit = (sp - bp)
+print("profit earned:", profit)
